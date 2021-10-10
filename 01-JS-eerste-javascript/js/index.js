c5546cb44efc796a528bb01bf2958a05bc9dff95
Celia Leijten <61545129+cleijten@users.noreply.github.com>
2021-10-10 15:58:58 +0200

    Add files via upload

---
+++ 01-JS-eerste-javascript/js/index.js
@@ -0,0 +1,18 @@
+      let firstName = "Celia";
+      let number1 = 6;
+      let number2 = 7;
+      let number3 = "6";
+      let number4 = "7";
+      let age = "53";
+
+      console.log(firstName);
+      console.log(number1 + number2);
+      console.log(number3 + number4); // Je kunt 2 strings bij elkaar optellen, dan wordt het een concatenation.
+      console.log(number1 - number2);
+      console.log(number1 * number1);
+      console.log(number3 * number4);
+      console.log(number3 / number1);
+      console.log(number2 / number3);
+      console.log(number1 % number2);
+console.log(typeof age);
+console.log(typeof (number3 + number4));
