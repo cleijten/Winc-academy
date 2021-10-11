3ef05704358f388327786408938d7885f8a919f2
Celia Leijten <61545129+cleijten@users.noreply.github.com>
2021-10-11 13:22:55 +0200

    Add files via upload

---
+++ 03-JS-IF-ELSE-STATEMENTS/index.js
@@ -0,0 +1,21 @@
+const visitorAge = 16;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (visitorAge >= 18) {
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Ho! Je bent jonger dan 18!");
+}
+
+if (isFemale) {
+    console.log("Welkom bij de ladies night mevrouw!");
+} else {
+    console.log("Sorry mannen mogen er vanavond niet in");
+}
+
+if (driverStatus == 'bob') {
+    console.log("Jij bent de BOB, jij mag rijden.");
+} else {
+    console.log("Jij mag niet rijden.");
+}
