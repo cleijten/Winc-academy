51c2d83a9fa47ba5e596ec7cf752dc00d5ba7b15
Celia Leijten <61545129+cleijten@users.noreply.github.com>
2021-10-11 12:34:03 +0200

    Add files via upload

---
+++ 02-JS-computer-comments/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten");
+// dit is een grote som!
+/* console.log(1230941 * 1823794);
+ console.log(637263 / 54); */
+
+ // Commentaar wordt niet getoond in de console. Zowel lijn commentaar als blok commentaar.
