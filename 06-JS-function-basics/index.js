659cd0e967bd0fa17060709fd3580836b48ba85a
Celia Leijten <61545129+cleijten@users.noreply.github.com>
2021-10-12 10:28:10 +0200

    Add files via upload

---
+++ 06-JS-function-basics/index.js
@@ -0,0 +1,27 @@
+//Maak pizza
+function makePizza() {
+  console.log("Verwarm de oven op 200 graden");
+  console.log("Maak deeg");
+  console.log("Maak de bodem");
+  console.log("Snij het pizza-beleg");
+  console.log("Bedek bodem met tomatensaus");
+  console.log("Leg het pizza-beleg op de bodem met tomatensaus");
+  console.log("Strooi geraspte kaas erover");
+  console.log("Zet hem in de oven");
+}
+
+//Maak sushi
+function makeSushi() {
+  console.log("Pak het rolmatje");
+  console.log("Leg het nori vel erop");
+  console.log("voeg sushirijst toe");
+  console.log("voeg komkommer, avocado, krab toe");
+  console.log("Rol op met het matje");
+}
+
+//Voer de functies uit
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
