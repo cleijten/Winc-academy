103c50ddb4fbc659016512b91a9789865589ab0e
Celia Leijten <61545129+cleijten@users.noreply.github.com>
2021-10-13 12:07:40 +0200

    Add files via upload

---
+++ 11-JS-loops-and-forloops/even-odd-reporter.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 20; i++) {
+    //console.log(`Is ${i} even or odd?`);
+    if (i % 2 === 0) {
+        //   console.log(i + " is even");
+        console.log(`${i} is even`);
+    } else {
+        // console.log(1 + " is oneven");
+        console.log(`${i} is oneven`);
+    }
+    
+}
